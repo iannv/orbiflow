c0b5dc1cd23e03e65837b1dc8a56248eed769fb2
Ian Vázquez <cam.iva@hotmail.com>
2026-04-28 21:49:29 -0300

    #TK000 componente button

---
+++ frontend/orbiflow/src/app/components/button/primary/primary.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-primary',
+  imports: [],
+  templateUrl: './primary.html',
+  styleUrl: './primary.css',
+})
+export class Primary {
+  @Input() name: string = '';
+}
