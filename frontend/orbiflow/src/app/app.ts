869bda21bebc4d856fedaa57e692df9f143ad916
Ian Vázquez <cam.iva@hotmail.com>
2026-05-07 20:29:18 -0300

    Sidebar

--- frontend/orbiflow/src/app/app.ts
+++ frontend/orbiflow/src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/header/header";
-import { Sidenav } from "./shared/sidenav/sidenav";
+import { Sidenav2 } from "./shared/sidenav2/sidenav2";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Sidenav],
+  imports: [RouterOutlet, Header, Sidenav2, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('orbiflow');
+  sidebarExpanded = true;
 }
