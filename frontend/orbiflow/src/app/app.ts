2c55df71d9e32372ecb33eab0c1e5e302952223b
Ian Vázquez <cam.iva@hotmail.com>
2026-05-08 22:25:04 -0300

    Componente card

--- frontend/orbiflow/src/app/app.ts
+++ frontend/orbiflow/src/app/app.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/header/header";
 import { Sidenav2 } from "./shared/sidenav2/sidenav2";
 import { NgClass } from "@angular/common";
-import { Chip } from './components/chip/chip';
+import { BaseCard } from "./components/base-card/base-card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Sidenav2, NgClass, Chip],
+  imports: [RouterOutlet, Header, Sidenav2, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
