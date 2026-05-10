1c80c2557fa3b0ee02255000bbe5ba4e93fd05a1
Ian Vázquez <cam.iva@hotmail.com>
2026-05-10 13:21:14 -0300

    Status

--- frontend/orbiflow/src/app/app.spec.ts
+++ frontend/orbiflow/src/app/app.spec.ts
@@ -15,10 +15,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  // it('should render title', async () => {
-  //   const fixture = TestBed.createComponent(App);
-  //   await fixture.whenStable();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain('Orbiflow');
-  // });
+  it('should render title', async () => {
+    const fixture = TestBed.createComponent(App);
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Orbiflow');
+  });
 });
