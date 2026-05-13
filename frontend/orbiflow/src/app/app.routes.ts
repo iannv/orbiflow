16ad3bab7b294c498ef3850030a6142b61802d78
Ian Vázquez <cam.iva@hotmail.com>
2026-05-12 22:34:46 -0300

    Page panel principal

--- frontend/orbiflow/src/app/app.routes.ts
+++ frontend/orbiflow/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PanelPrincipal } from './pages/panel-principal/panel-principal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/panel-principal', pathMatch: 'full' },
+  { path: '**', component: PanelPrincipal },
+  { path: 'panel-principal', component: PanelPrincipal },
+];
