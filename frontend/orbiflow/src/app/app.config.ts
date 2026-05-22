a149dbd4ad4d1b22374b264920fce3a1ef0eab48
Ian Vázquez <cam.iva@hotmail.com>
2026-05-22 17:39:04 -0300

    implementacion vercel

--- frontend/orbiflow/src/app/app.config.ts
+++ frontend/orbiflow/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/auth/auth.interceptor';
 
@@ -12,3 +12,5 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
+
+export const API_BASE_URL = environment.apiUrl;
---
+++ frontend/orbiflow/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://orbiflow-backend-sandbox.onrender.com/api' // URL de producción
+};
