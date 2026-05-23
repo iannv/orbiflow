96b5b62b1f7b644cf8471237e3c98d9747e736a6
Ian Vázquez <cam.iva@hotmail.com>
2026-05-23 00:49:57 -0300

    Mis recibos - collapse

--- frontend/orbiflow/src/app/pages/recibos/recibos.ts
+++ frontend/orbiflow/src/app/pages/recibos/recibos.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { BaseCard } from "../../components/base-card/base-card";
 
 @Component({
   selector: 'app-recibos',
-  imports: [],
+  imports: [BaseCard],
   templateUrl: './recibos.html',
   styleUrl: './recibos.css',
 })
-export class Recibos {}
+export class Recibos {
+  year: string = '';
+  arrow: string = 'assets/flecha-abajo.png'; // Si collapse false la flecha es hacia la derecha
+
+  collapse: boolean = false;
+
+  // ngOnInit(){
+  //   this.isCollapse();
+  // }
+
+  // isCollapse() {
+  //   this.collapse = this.collapse != this.collapse;
+  //   this.arrow = 'assets/flecha-derecha.png';
+  // }
+}
