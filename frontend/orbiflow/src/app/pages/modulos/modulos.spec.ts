ad068ad76341e562f7b2c322edc90e1895f837eb
Ian Vázquez <cam.iva@hotmail.com>
2026-06-19 22:32:33 -0300

    Form nuevo módulo validaciones

--- frontend/orbiflow/src/app/pages/modulos/modulos.spec.ts
+++ frontend/orbiflow/src/app/pages/modulos/modulos.spec.ts
@@ -86,14 +86,13 @@ describe('Modulos Component', () => {
     expect(component.variantesFormArray.length).toBe(1);
   });
 
-  it('NO debe guardar el módulo si no posee al menos una variante y debe mostrar mensaje de error', () => {
+  it('NO debe guardar el módulo si no posee al menos una variante', () => {
     const spyCreate = vi.spyOn(mockModulosService, 'createModulo');
     component.variantesFormArray.clear();
     component.guardarModulo();
     expect(spyCreate).not.toHaveBeenCalled();
-    expect(component.mgeError).toBe(
-      'Debe agregar al menos una variante al módulo para realizar cálculos.',
-    );
+    expect(component.variantesFormArray.invalid).toBe(true);
+    expect(component.variantesFormArray.hasError('minlength')).toBe(true);
   });
 
   it('NO debe guardar si una variante de tipo porcentaje supera el 100%', () => {
